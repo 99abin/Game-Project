8b2c1322defef7c7e69849b1fca93ad8c45e4403
Abin <bintanghidayatullah021@gmail.com>
2025-08-08 08:09:10 +0700

    Update main.js

--- RockPaperScissor/Simple/main.js
+++ RockPaperScissor/Simple/main.js
@@ -1,6 +1,12 @@
+let score = JSON.parse(localStorage.getItem('score')) || {
+    user: 0,
+    computer: 0,
+    ties: 0
+};
+
 function calculate(pickedMove) {
     const random = Math.random()
-    let userMove = pickedMove ; let result = ''; compMove = ''
+    let userMove = pickedMove ; let result = ''; let compMove = ''
 
     if (random > 0 && random < 1/3) {
         compMove = 'rock'
@@ -21,5 +27,33 @@ function calculate(pickedMove) {
         result = 'draw'
     }
 
-    alert(`you picked ${userMove}, computer picked ${compMove}, you ${result}`)
-}
+    if (result === 'win') {
+        score.user++
+    } else if (result === 'lose') {
+        score.computer++
+    } else if (result === 'draw') {
+        score.ties++
+    }
+
+    localStorage.setItem('score', JSON.stringify(score))
+    const score1 = JSON.parse(localStorage.getItem('score'))
+
+    document.querySelector('.move').textContent = `You: ${userMove}, Computer: ${compMove}`
+    document.querySelector('.rslt').textContent = `You ${result}`
+    document.querySelector('.scr').textContent = `You: ${score1.user}, Computer: ${score1.computer}, Ties: ${score1.ties}`
+} 
+
+function resetScore() {
+    localStorage.removeItem('score')
+    score = {
+        user: 0,
+        computer: 0,
+        ties: 0
+    }
+    document.querySelector('.move').textContent = `You: -, Computer: -`
+    document.querySelector('.rslt').textContent = `You -`
+    document.querySelector('.scr').textContent = `You: 0, Computer: 0, Ties: 0`
+} 
+
+
+
